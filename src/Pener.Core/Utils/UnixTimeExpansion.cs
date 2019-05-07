54bf85354f67c66a2644df414d61c508af623a32
SUIMA0310 <mizumacrocus@yahoo.co.jp>
2019-05-07 22:23:18 +0900

    add UnixTime helper.

---
+++ src/Pener.Core/Utils/UnixTimeExpansion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pener.Utils
+{
+    public static class UnixTimeExpansion
+    {
+        private static DateTime BASE_TIME = new DateTime(1970, 1, 1);
+
+        public static DateTime FromUnixTime(this int unixTime)
+        {
+            return BASE_TIME.AddSeconds(unixTime).ToLocalTime();
+        }
+
+        public static int ToUnixTime(this DateTime time)
+        {
+            return (int)(time.ToUniversalTime() - BASE_TIME).TotalSeconds;
+        }
+
+    }
+}
