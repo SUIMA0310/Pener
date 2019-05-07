ebc5a5b9a4fc3bff16ba08c715323ad951666d6c
SUIMA0310 <mizumacrocus@yahoo.co.jp>
2019-05-07 22:21:48 +0900

    Login Command を実装

--- src/Pener.Client.Cli/Commands/Auth/LoginCommand.cs
+++ src/Pener.Client.Cli/Commands/Auth/LoginCommand.cs
@@ -1,12 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using McMaster.Extensions.CommandLineUtils;
+using Pener.Client.Services.Auth;
 
 namespace Pener.Client.Cli.Commands.Auth
 {
     [Command]
     public class LoginCommand
     {
+        private readonly IAuthService _authService;
+
+        [Argument(0)]
+        public string UserName { get; private set; }
+
+        [Option("-p | --password")]
+        public string Password { get; private set; }
+
+        public LoginCommand(
+            IAuthService loginService)
+        {
+            _authService = loginService;
+        }
+
+        public async Task OnExecute(CommandLineApplication app)
+        {
+            if (UserName == null)
+            {
+                UserName = Prompt.GetString("UserName : ");
+            }
+
+            if (Password == null)
+            {
+                Password = Prompt.GetPassword("Password : ");
+            }
+
+            try
+            {
+                await _authService.LoginAsync(UserName, Password);
+
+                Console.WriteLine(Consts.Messages.Success);
+            }
+            catch
+            {
+                Console.WriteLine(Consts.Messages.Failure);
+            }
+        }
     }
 }
--- src/Pener.Client.Cli/Commands/Auth/LogoutCommand.cs
+++ src/Pener.Client.Cli/Commands/Auth/LogoutCommand.cs
@@ -1,12 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using McMaster.Extensions.CommandLineUtils;
+using Pener.Client.Services.Auth;
+using Pener.Client.Services.Jwt;
 
 namespace Pener.Client.Cli.Commands.Auth
 {
     [Command]
     public class LogoutCommand
     {
+        private readonly IAuthService _authService;
+
+        public LogoutCommand(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        public async Task OnExecute(CommandLineApplication app)
+        {
+            try
+            {
+                await _authService.LogoutAsync();
+
+                Console.WriteLine(Consts.Messages.Success);
+            }
+            catch
+            {
+                Console.WriteLine(Consts.Messages.Failure);
+            }
+        }
     }
 }
