d5957fa4c81e48f270b183c47f9ece8c5364a17c
Takuya Mizuma <mizumacrocus@yahoo.co.jp>
2019-05-13 12:45:01 +0900

    add Error responce models.

---
+++ src/Pener.Core/Models/Error.cs
@@ -0,0 +1,15 @@
+﻿namespace Pener.Models
+{
+    public class Error
+    {
+        /// <summary>
+        /// エラーの発生個所を示すためのID情報
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// エラーに対するメッセージ
+        /// </summary>
+        public string Message { get; set; }
+    }
+}
---
+++ src/Pener.Core/Models/ErrorResponce.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace Pener.Models
+{
+    /// <summary>
+    /// Requestの処理中にエラーが発生した際の応答
+    /// </summary>
+    public class ErrorResponce
+    {
+        /// <summary>
+        /// エラーの詳細情報
+        /// </summary>
+        public IEnumerable<Error> Errors { get; set; }
+    }
+}
